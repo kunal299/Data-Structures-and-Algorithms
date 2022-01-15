a5a2125ee3f612636b8e59370af97bc5a0a9d71e
kunal299 <kunalmali299@gmail.com>
2022-01-15 10:18:18 +0530

    Added Comments to BinaryTreeNode.h

--- Data Structures/Binary Trees/1. Basics/BinaryTreeNode.h
+++ Data Structures/Binary Trees/1. Basics/BinaryTreeNode.h
@@ -15,9 +15,9 @@ public:
 	//Destructor
 	~BinaryTreeNode() {
 		/*
-			Don't worry about the case where there is no left and right.
-			As we have initiated left and right with NULL value in the constructor, 
-			in that case it will call delete NULL, which has no effect and is ignored.
+		Don't worry about the case where there is no left and right.
+		As we have initiated left and right with NULL value in the constructor, 
+		in that case it will call delete NULL, which has no effect and is ignored.
 		*/
 		delete left;
 		delete right;
