29248857a3b107a5b50f6dabefb5115d8d6a92b0
kunal299 <kunalmali299@gmail.com>
2022-01-15 09:50:26 +0530

    Added BinaryTreeNode.h

---
+++ Data Structures/Binary Trees/BinaryTreeNode.h
@@ -0,0 +1,25 @@
+template <typename T>
+class BinaryTreeNode{
+public:
+	T data;
+	BinaryTreeNode* left;
+	BinaryTreeNode* right;
+
+	//Constructor
+	BinaryTreeNode(T data) {
+		this->data = data;
+		left = NULL;
+		right = NULL;
+	}
+
+	//Destructor
+	~BinaryTreeNode() {
+		/*
+			Don't worry about the case where there is no left and right.
+			As we have initiated left and right with NULL value in the constructor, 
+			in that case it will call delete NULL, which has no effect and is ignored.
+		*/
+		delete left;
+		delete right;
+	}
+};
