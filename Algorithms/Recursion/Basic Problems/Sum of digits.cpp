f30a3d5cfc4c7d57c6e09e7d064c823e99e34308
kunal299 <kunalmali299@gmail.com>
2022-01-19 22:33:44 +0530

    Sum of digits.cpp

---
+++ Algorithms/Recursion/Basic Problems/Sum of digits.cpp
@@ -0,0 +1,33 @@
+/*
+Sum of digits (recursive)
+
+Write a recursive function that returns the sum of the digits of a given integer.
+
+Input format :
+Integer N
+
+Output format :
+Sum of digits of N
+
+Constraints :
+0 <= N <= 10^9
+
+Sample Input 1 :
+12345
+Sample Output 1 :
+15
+
+Sample Input 2 :
+9
+Sample Output 2 :
+9
+*/
+
+int sumOfDigits(int n) {
+	if(n==0) {
+		return 0;
+	}
+
+	//Adding the last digit with the recursive calls to get the total sum 
+	return (n%10) + sumOfDigits(n/10);
+}
