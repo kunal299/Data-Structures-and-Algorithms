b174da2dd26288aae8490b173d1631149e1e3e40
Kunal Mali <71486434+kunal299@users.noreply.github.com>
2022-01-20 19:36:10 +0530

    Update Search in BST.cpp

--- Data Structures/Binary Search Trees/1. Basics/Search in BST.cpp
+++ Data Structures/Binary Search Trees/1. Basics/Search in BST.cpp
@@ -1,11 +1,15 @@
 /*
 Code: Search in BST
 
-Given a BST and an integer k. Find if the integer k is present in given BST or not. You have to return true, if node with data k is present, return false otherwise.
+Given a BST and an integer k. Find if the integer k is present in given BST or not. 
+You have to return true, if node with data k is present, return false otherwise.
 Note: Assume that BST contains all unique elements.
 
 Input Format:
-The first line of input contains data of the nodes of the tree in level order form. The data of the nodes of the tree is separated by space. If any node does not have left or right child, take -1 in its place. Since -1 is used as an indication whether the left or right nodes exist, therefore, it will not be a part of the data of any node.   
+The first line of input contains data of the nodes of the tree in level order form. 
+The data of the nodes of the tree is separated by space. 
+If any node does not have left or right child, take -1 in its place. 
+Since -1 is used as an indication whether the left or right nodes exist, therefore, it will not be a part of the data of any node.   
 The following line of input contains an integer, that denotes the value of k.
 
 Output Format:
