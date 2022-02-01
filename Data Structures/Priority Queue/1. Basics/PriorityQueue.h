34f67b54f1d875d83c9bb93509defa3edb1103b7
kunal299 <kunalmali299@gmail.com>
2022-02-01 16:39:17 +0530

    Update PriorityQueue.h

--- Data Structures/Priority Queue/1. Basics/PriorityQueue.h
+++ Data Structures/Priority Queue/1. Basics/PriorityQueue.h
@@ -34,13 +34,55 @@ public:
 
 			if(pq[childIndex]<pq[parentIndex]) {
 				swap(pq[childIndex], pq[parentIndex]);
+			} else {
+				break;
 			}
 
 			childIndex = parentIndex;
 		}
+	}
 
+	int removeMin() {
+		if(pq.isEmpty()) {
+			return 0; //Priority Queue is empty
+		}
 
+		int ans = pq[0];
+		int lastIndex = pq.size()-1;
 
+		swap(pq[0], pq[lastIndex]);
+		pq.pop_back();
+
+		//down-heapify
+
+		int parentIndex = 0;
+		int leftChildIndex = (2*parentIndex) + 1;
+		int rightChildIndex = (2*parentIndex) + 2;
+
+		//Used leftChildIndex here as there would be no right child unless there is a left child.
+		while(leftChildIndex < pq.size()) {
+			int minIndex = 0;
+			if(pq[minIndex] > pq[leftChildIndex]) {
+				minIndex = leftChildIndex;
+			}
+
+			if(pq[minIndex] > pq[rightChildIndex] && rightChildIndex<pq.size()) {
+				minIndex = rightChildIndex;
+			}
+
+			//If we reach at the proper place
+			if(minIndex==parentIndex) {
+				break;
+			}
+
+			swap(pq[minIndex], pq[parentIndex]);
+
+			parentIndex = minIndex;
+			leftChildIndex = (2*parentIndex) + 1;
+			rightChildIndex = (2*parentIndex) + 2;
+		}
+
+		return ans;		
 	}
 
 };
