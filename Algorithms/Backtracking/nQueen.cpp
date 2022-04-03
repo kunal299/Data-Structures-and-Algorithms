5f6000c27125534d7d1c4d842c16e59c26f82bf2
kunal299 <kunalmali299@gmail.com>
2022-04-03 10:36:06 +0530

    Added comments to nQueen.cpp

--- Algorithms/Backtracking/nQueen.cpp
+++ Algorithms/Backtracking/nQueen.cpp
@@ -1,5 +1,39 @@
+/*
+N-Queen Problem
+
+You are given N, and for a given N x N chessboard, find a way to place N queens such that no queen can attack any other queen on the chess board. 
+A queen can be killed when it lies in the same row, or same column, or the same diagonal of any of the other queens. 
+You have to print all such configurations.
+Note: Don't print anything if there isn't any valid configuration.
+
+Input Format:
+The first line of input contains an integer, that denotes the value of N.     
+
+Output Format :
+In the output, you have to print every board configuration in a new line. Every configuration will have N*N board elements printed row wise and are separated by space. The cells where queens are safe and placed, are denoted by value 1 and remaining all cells are denoted by value 0. Please refer to sample test cases for more clarity.
+
+Constraints :
+1 <= N <= 10
+Time Limit: 1 second
+
+Sample Input 1:
+4
+Sample Output 1 :
+0 1 0 0 0 0 0 1 1 0 0 0 0 0 1 0 
+0 0 1 0 1 0 0 0 0 0 0 1 0 1 0 0 
+Explanation:
+The first and second configuration are shown in the image below. Here, 1 denotes the placement of a queen and 0 denotes an empty cell. Please note that in both the configurations, no pair of queens can kill each other. 
+*/
+
 #include "bits/stdc++.h"
 using namespace std;
+
+/*
+	Time Complexity - O(N*2^N)
+	Space Complexity - O(N*N)
+
+	where N is the input size of the chess board
+*/
 	
 int board[11][11];
 
