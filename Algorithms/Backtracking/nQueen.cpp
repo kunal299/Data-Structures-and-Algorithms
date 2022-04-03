8346f6bc72f9a7eb74b08a93e145fb9640678512
kunal299 <kunalmali299@gmail.com>
2022-04-03 07:58:31 +0530

    Added nQueen.cpp

---
+++ Algorithms/Backtracking/nQueen.cpp
@@ -0,0 +1,65 @@
+#include "bits/stdc++.h"
+using namespace std;
+	
+int board[11][11];
+
+bool isPossible(int n, int row, int col) {
+
+	//Same Column
+	for(int i=row-1; i>=0; i--) {
+		if(board[i][col]==1) {
+			return false;
+		}
+	}
+
+	//Upper Left Diagonal
+	for(int i=row-1, j=col-1; i>=0 && j>=0; i--, j--) {
+		if(board[i][j]==1) {
+			return false;
+		}
+	}
+
+	//Upper Right Diagonal
+	for(int i=row-1, j=col+1; i>=0 && j<n; i--, j++) {
+		if(board[i][j]==1) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+void nQueenHelper(int n, int row) {
+	if(n==row) {
+		//We have reached some solution.
+		//Print the board matrix
+		//return
+		for(int i=0; i<row; i++) {
+			for(int j=0; j<n; j++) {
+				cout<<board[i][j]<<" ";
+			}
+		}
+		cout<<"\n";
+		return;
+	}
+
+	//Place at all possible positions and move to smaller problem
+	for(int j=0; j<n; j++) {
+		if(isPossible(n, row, j)) {
+			board[row][j] = 1;
+			nQueenHelper(n, row+1);
+			board[row][j] = 0;
+		}
+	}
+	
+
+}
+
+void placeNQueen(int n) {
+	memset(board, 0, 11*11*sizeof(int));
+	nQueenHelper(n, 0);
+}
+
+int main() {
+	placeNQueen(4);
+}
