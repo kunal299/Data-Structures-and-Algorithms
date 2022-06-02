bea87eda33cb40e6b3623bdc2ca9553e9989dd73
kunal299 <kunalmali299@gmail.com>
2022-06-02 08:23:06 +0530

    Added Pascal's Triangle

---
+++ SDE Sheet by Striver/1. Arrays/2. Pascal's Triangle/Approach - 1.cpp
@@ -0,0 +1,26 @@
+/*
+    Time Complexity - O(N^2)
+    Space Complexity - O(N^2)
+
+    where N - no. of rows
+*/
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> v(numRows);
+        
+        for(int i=0; i<numRows; i++) {
+            v[i].resize(i+1);
+            v[i][0] = 1;
+            v[i][i] = 1;
+            
+            for(int j=1; j<i; j++) {
+                v[i][j] = v[i-1][j-1] + v[i-1][j];
+            }
+        }
+        
+        return v;
+        
+    }
+};
