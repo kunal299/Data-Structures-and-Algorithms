8a1031fbcd3bbd3f5f776ee3bfc98dec5a3acde2
kunal299 <kunalmali299@gmail.com>
2022-06-11 16:33:22 +0530

    Added Search a 2D Matrix

--- SDE Sheet by Striver/3. Arrays Part-III/1. Search a 2D Matrix/Approach - 2.cpp
+++ SDE Sheet by Striver/3. Arrays Part-III/1. Search a 2D Matrix/Approach - 2.cpp
@@ -7,20 +7,16 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         
-        int row = 0;
-        int i;
-        for(i=1; i<matrix.size(); i++) {
-            if(matrix[i][0]>target) {
-                row = i-1;
-                break;
-            } else if(target>=matrix[i][0]) {
-                row = i;
-            }
-        }
-        
-        for(i=0; i<matrix[row].size(); i++) {
-            if(matrix[row][i]==target) {
+        int n = matrix.size(), m = matrix[0].size();
+        int i = 0, j = m-1;
+
+        while((i<n) && (j>=0)) {
+            if(matrix[i][j] == target) {
                 return true;
+            } else if(matrix[i][j] > target)  {
+                j--;
+            } else {
+                i++;
             }
         }
         
