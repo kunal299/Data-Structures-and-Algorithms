189b92b21ce085359493621a0b398030233cf714
Kunal Mali <kunalmali299@gmail.com>
2022-07-08 18:13:14 +0530

    Modified Minimum Number of Platforms

--- SDE Sheet by Striver/8. Greedy Algorithm/2. Minimum Number of Platforms/Approach - 1.cpp
+++ SDE Sheet by Striver/8. Greedy Algorithm/2. Minimum Number of Platforms/Approach - 1.cpp
@@ -3,25 +3,33 @@
     Space Complexity - O(1)
 */
 
-int calculateMinPatforms(int at[], int dt[], int n) {
-    sort(at, at+n);
-    sort(dt, dt+n);
-    
-    int maxi = 1;
-    int plat = 1;
-    
-    int i=1, j=0;
-    while(i<n && j<n) {
-        if(at[i] <= dt[j]) {
-            plat++;
-            i++;
-        } else {
-            plat--;
-            j++;
+class Solution{
+    public:
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    int findPlatform(int arr[], int dep[], int n)
+    {
+        sort(arr, arr+n);
+        sort(dep, dep+n);
+        
+        int i=0;
+        int j=0;
+        
+        int maxTrain = 0;
+        int plat = 0;
+        
+        while(i<n && j<n) {
+            if(arr[i] <= dep[j]) {
+                maxTrain++;
+                i++;
+            } else {
+                maxTrain--;
+                j++;
+            }
+            
+            plat = max(plat, maxTrain);
         }
         
-        maxi = max(maxi, plat);
+        return plat;
     }
-    
-    return maxi;
-}
+};
