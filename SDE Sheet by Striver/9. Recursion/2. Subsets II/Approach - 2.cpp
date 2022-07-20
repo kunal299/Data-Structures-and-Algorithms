06a91a1a76d160be4c6f53c060375df3f7a65f7f
Kunal Mali <kunalmali299@gmail.com>
2022-07-20 09:00:59 +0530

    Modified Subsets II

--- SDE Sheet by Striver/9. Recursion/2. Subsets II/Approach - 2.cpp
+++ SDE Sheet by Striver/9. Recursion/2. Subsets II/Approach - 2.cpp
@@ -6,23 +6,23 @@
 
 class Solution {
 public:
-    void helper(int idx, vector<int> &nums, int n, vector<vector<int>> &st, vector<int> &res) {
-        st.push_back(res);
+    void helper(int idx, vector<int> &nums, int n, vector<vector<int>> &vv, vector<int> &res) {
+        vv.push_back(res);
         for(int i=idx; i<nums.size(); i++) {
             if(i!=idx && nums[i]==nums[i-1]) continue;
     
             res.push_back(nums[i]);
-            helper(i+1, nums, n, st, res);
+            helper(i+1, nums, n, vv, res);
             res.pop_back();  
         }
     }
     
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<int> res;
-        vector<vector<int>> st;
+        vector<vector<int>> vv;
         sort(nums.begin(), nums.end());
-        helper(0, nums, nums.size(), st, res);
+        helper(0, nums, nums.size(), vv, res);
         
-        return st;
+        return vv;
     }
 };
