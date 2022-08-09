2620e3caf705cc5f592133105ab84d93c1b43c1f
Kunal Mali <kunalmali299@gmail.com>
2022-08-09 16:59:00 +0530

    Added Approach - 2 of N-Queens

---
+++ SDE Sheet by Striver/Recursion and Backtracking/2. N-Queens/Approach - 2.cpp
@@ -0,0 +1,43 @@
+/*
+    Time Complexity - O(n! * n)
+    Space Complexity - O(n)
+*/
+
+class Solution {
+public:
+    void helper(int col, vector<vector<string>> &ans, vector<string> &board, vector<int> &leftRow, vector<int> &upperDiagonal, vector<int> &lowerDiagonal, int n) {
+        if(col == n) {
+            ans.push_back(board);
+            return;
+        }
+        
+        for(int row=0; row<n; row++) {
+            if(leftRow[row]==0 && lowerDiagonal[row+col]==0 && upperDiagonal[n-1 + col-row]==0) {
+                board[row][col] = 'Q';
+                leftRow[row] = 1;
+                lowerDiagonal[row+col] = 1;
+                upperDiagonal[n-1 + col-row] = 1;
+                helper(col+1, ans, board, leftRow, upperDiagonal, lowerDiagonal, n);
+                board[row][col] = '.';
+                leftRow[row] = 0;
+                lowerDiagonal[row+col] = 0;
+                upperDiagonal[n-1 + col-row] = 0;
+            }
+        }
+    }
+    
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n);
+        string s(n, '.');
+         
+        for(int i=0; i<n; i++) {
+            board[i] = s;
+        }
+        
+        vector<int> leftRow(n, 0), upperDiagonal((2*n)-1, 0), lowerDiagonal((2*n)-1, 0);
+        
+        helper(0, ans, board, leftRow, upperDiagonal, lowerDiagonal, n);
+        return ans;
+    }
+};
